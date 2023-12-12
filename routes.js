e324d34999f4b0e41e32cf3eaa65c317371935e4
tushar-fundflo <tushar.vitole@fundflo.ai>
2023-12-12 12:13:44 +0530

    name

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 module.exports = function (app) {
     app.get('/', (request,response)=>{
-        response.status(200).json({status: false, message: 'Welcome to Fundflo Bank Integration '});
+        response.status(200).json({status: false, message: 'Welcome 2 to Fundflo Bank Integration '});
         return;
     }),
     app.use('/canara', require('./canara/canaraBank.routes'));
